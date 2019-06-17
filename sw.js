5838fbb32dc7814ea1a0c13fad93c31a0246c867
Juan Pablo Quitian <juanp111421@gmail.com>
2019-06-17 14:05:57 -0500

    Modificamos par vista en IOS

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //importacion
 importScripts('js/sw-utils.js');
 
-const CACHE_STATIC    = 'static-v1';
+const CACHE_STATIC    = 'static-v2';
 const CACHE_DYNAMIC   = 'dynamic-v1';
 const CACHE_INMUTABLE = 'inmutable-v1';
 
